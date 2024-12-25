4bc593fcf5c7d91b9abafeddd2e9d0fa75380893
maryambsq <maryamx399@gmail.com>
2024-12-25 12:36:41 +0300

    enhanced enlarged grouped keys layout

---
+++ EnglishKeyboardView/MainEnglishKeyboard.swift
@@ -0,0 +1,18 @@
+//
+//  MainEnglishKeyboard.swift
+//  EnglishKeyboardView
+//
+//  Created by Maryam Amer Bin Siddique on 24/06/1446 AH.
+//
+
+import SwiftUI
+
+struct MainEnglishKeyboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainEnglishKeyboard()
+}
